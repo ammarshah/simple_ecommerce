05f71ec600d54ef22d8d8271c300f19e6aab4d93
Ammar Shah <ammarshah123@gmail.com>
2016-12-09 12:43:57 +0500

    Show latest order first.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  	@orders = Order.by_user(current_user).placed.order('date DESC')
+  	@orders = Order.by_user(current_user).placed.order(date: :desc, id: :desc)
   end
 end
