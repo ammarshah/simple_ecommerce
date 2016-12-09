d236e956c97472b7256d764a597f42bad19741c0
Ammar Shah <ammarshah123@gmail.com>
2016-12-09 15:23:34 +0500

    Price will be a double, c'mon!

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :order_lines
 
   validates_presence_of :name, :description, :status
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   scope :enabled, -> { where(status: 1) }
 
