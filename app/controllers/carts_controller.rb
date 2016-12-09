b36db189b583a5e81f4d97d018d9607b840513c6
Ammar Shah <ammarshah123@gmail.com>
2016-12-09 10:01:39 +0500

    Update nav bar

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
   	@order_lines = current_order.order_lines
   end
--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,4 +1,6 @@
 class OrderLinesController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
     @order = current_order
     
